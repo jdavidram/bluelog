98b50158b48bb726c4a47d5e674283418ed9f4ac
Juanjolk <juagarciava@unal.edu.co>
2024-09-08 16:00:28 -0500

    Login con back

--- src/App/Login/Login.jsx
+++ src/App/Login/Login.jsx
@@ -1,41 +1,56 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as Logo } from "../logo.svg";
+import axios from 'axios';
 import "./Login.scss";
-import data from "../data.json";
-
-function validUser(targetValue) {
-    var username = document.getElementById("username").value;
-    if (data[username] !== undefined && data[username].password === targetValue) {
-        return "projects";
-    } else {
-        return "";
-    }
-}
-
-function wrongPass(valid) {
-    if (valid === "") {
-        alert("Usuario o contraseña incorrectos");
-    }
-}
 
 function Login() {
-    const [valid, setValid] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/login', { username, password });
+            if (response.data.status === "success") {
+                navigate("/projects");// Redirige al usuario a la página de proyectos
+                //setMessage('Login exitoso')  
+            } else {
+                setError("Usuario o contraseña incorrectos");
+            }
+        } catch (error) {
+            setError("Error al comunicarse con el servidor");
+        }
+    };
+
     return (
         <div id="login">
             <Logo />
-            <form action="" method="get">
+            <form onSubmit={handleLogin}>
                 <label htmlFor="username">
-                    <input type="text" name="username" id="username" placeholder="username" />
+                    <input 
+                        type="text" 
+                        name="username" 
+                        id="username" 
+                        placeholder="username" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </label>
                 <label htmlFor="password">
-                    <input type="password" name="password" id="password" placeholder="password" onChange={(event) => { 
-                        setValid(validUser(event.target.value));
-                     }} />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
-                <NavLink to={ "/" + valid }>
-                    <button className="button" type="submit" onClick={ () => wrongPass(valid) }>Login</button>
-                </NavLink>
+                <button className="button" type="submit">Login</button>
+                {error && <p>{error}</p>}
             </form>
         </div>
     );
