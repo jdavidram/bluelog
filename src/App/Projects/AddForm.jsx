bbe36eaf5eb9aa47edc40eb501350108d0150821
JDavid Ram <150093295+jdavidram@users.noreply.github.com>
2024-09-08 17:38:24 -0500

    Update AddForm.jsx

--- src/App/Projects/AddForm.jsx
+++ src/App/Projects/AddForm.jsx
@@ -9,6 +9,9 @@ function AddForm({ folders, setFolders }) {
     const addFolder = () => {
         let name = document.getElementById("name").value;
         let img = document.getElementById("image").value.replaceAll("\\", "/");
+        if (img === "") {
+            img = "https://estaticos.elcolombiano.com/binrepository/893x565/57c0/780d565/none/11101/XKEB/documentation-fotos-1-13717159-6ed56232effbbe3127a19d67b2f920b4_43070691_20230817105411.jpg";
+        }
         setFolders([...folders, {
             "name": name,
             "image": img
